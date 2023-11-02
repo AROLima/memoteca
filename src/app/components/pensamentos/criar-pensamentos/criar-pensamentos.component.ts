8f31a3a7e1e553fc313254d40badf74dac7cb782
Rodrigo <rodrigo.lima546@hotmail.com>
2023-11-02 04:17:55 -0300

    Addicinando property binding

--- src/app/components/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
+++ src/app/components/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class CriarPensamentosComponent {
 
+  pensamento = {
+    id: "1",
+    conteudo: "aprendendo angular",
+    autoria: "dev",
+    modelo: ""
+  }
+
 }
